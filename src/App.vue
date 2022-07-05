3a0cf5cee732b1b7787ec1f4b29a9cbcaa5da788
LucioGhedo <lucioghedina@gmail.com>
2022-07-05 14:17:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="La mia prima app con Vue CLI!"/>
+    <TestCompo msg="Il primo jumbotron component con VUE CLI" personal-msg="doppio msg wow"/>
+    <MyCompo test="img"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TestCompo from './components/TestCompo.vue'
+import MyCompo from './components/MyCompo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TestCompo,
+    MyCompo
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TestCompo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>{{ personalMsg }}</div>
+    <section>
+      <div class="jumbotron">
+        <h2>{{ msg }}</h2>
+        <img src="https://picsum.photos/200/200" alt="lorem picsum">
+        <div>jumbotron</div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestCompo',
+  props: {
+    msg: String,
+    personalMsg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+.jumbotron {
+  background-color: #42b983;
+  padding: 50px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
